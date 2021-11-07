4f2dc97d5b17bbbbb1e809b0b15346cd5d04e87e
JonathWesley <jonathhherdt@gmail.com>
2021-11-07 02:01:37 -0300

    adicionando mais atores

--- coffee/coffee.pl
+++ coffee/coffee.pl
@@ -1,15 +1,19 @@
-:-dynamic have/1, % indicates that we have a product ready for use.
+:-dynamic 
+have/1, % indicates that we have a product ready for use.
 canProduce/1, % indicates products that we can make ourselves.
 canProduce/2, % indicates that another machine can make a certain product.
 delivered/2. % indicates that a product has been delivered to a certain machine.
+
 % Common knowledge of ingredients that are needed for making a product.
 requiredFor(coffee, water).
 requiredFor(coffee, grounds).
 requiredFor(espresso, coffee).
 requiredFor(grounds, beans).
+
 % A machine can make a product if it has the product or it can make all ingredients.
 canMake(Product) :- have(Product).
 canMake(Product) :- canProduce(Product),
 forall(requiredFor(Product, Ingredient), canMake(Ingredient)).
+
 % A short hand for having delivered a product to any machine.
 delivered(Product) :- delivered(_,Product).
---
+++ fight/fight.pl
@@ -0,0 +1,20 @@
+:-dynamic 
+has/1, % indicates that we have a weapon ready for use.
+canAtack/2, % indicates we can atack someone else.
+canDefend/1, % indicates that we can defend ourselves.
+canDodge/1. % indicates that we can dodge an atack.
+nrOfLifes/1. % indicates how much life an agent has.
+kill/1. % indicates we can kill someone else.
+
+% Common knowledge of intes that are needed for moves
+requiredFor(atack, weapon).
+requiredFor(defense, shield).
+requiredFor(dodge, boot).
+% Common knowledge of itens that are needed for making a flag.
+requiredFor(flag, pieceOne).
+requiredFor(flag, pieceTwo).
+requiredFor(flag, pieceThree).
+
+% A machine can make a product if it has the product or it can make all ingredients.
+canCraft(Product) :- has(Product).
+forall(requiredFor(Product, Ingredient)).
